0dc008e7926e6bc18be458736b37d7484b372d6a
CharmaineAkodi <51997303+CharmaineAkodi@users.noreply.github.com>
2020-03-05 11:57:03 +0100

    Final

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Readme voor Dashboard_Challenge 1
 Dit is de readme file voor repo Dashboard_Challenge 1
+
+Er is een responsive dashboard gemaakt om de data van de reis naar Mars weer te geven. Het is responsive gemaakt, zodat dit dashboard op verschillende schermgroottes te bekijken is.
+
+In dit dashboard is de volgende informatie te zien:
+
+- Snelheid
+- G-Force
+- Aantal passagiers
+- Brandstof
+- Afstand afgelegd over tijd
+- De bevoorading aan boord
+
+Er is bij het maken van dit dashboard vooral focus gelegd op de astronauten die het ruimteschip zullen besturen. Daarom is er gekozen voor informatie dat voor hen van belang kan zijn tijdens het maken van deze reis.
+
+(Helaas is het niet gelukt alles volledig responsive te krijgen. Niet alle mediaqueries willen reageren wanneer het scherm word aangepast in Google Chrome. In safari werken ze wel, maar ook daar werkt niet alle code)
--- scripts/charts.js
+++ scripts/charts.js
@@ -1,74 +1,77 @@
-new Chart(document.getElementById("fuelChart"), {
-    type: 'doughnut',
-    data: {
-      labels: ["Remaining", "Used"],
-      datasets: [
-        {
-          data: [79,21],
-          backgroundColor: ["#005288", "#DCEDF9"]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Fuel left in percentage'
-      }
-    }
-});
-
-new Chart(document.getElementById("distanceChart"), {
-  type: 'line',
-  data: {
-    labels: [10,20,30,40,50,60,70,80,90,100],
-    datasets: [{ 
-        data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Spaceship",
-        borderColor: "#005288",
-        backgroundColor: ["#DCEDF9"]
-      }
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Distance traveled over time'
-    }
-  }
-});
-
-new Chart(document.getElementById('supplyChart'), {
-    type: 'bar',
-   	data: {
-        labels: ['Bread', 'Rice', 'Vegetables', 'Meat', 'Fruit'],
-        datasets: [{
-            data: [78, 76,82 ,69,75],
-            label: 'Percentage of supply left',
-            backgroundColor: [
-                'rgb(204, 220, 231)',
-                'rgb(204, 220, 231)',
-                'rgb(204, 220, 231)',
-                'rgb(204, 220, 231)',
-                'rgb(204, 220, 231)'
-            	],
-            borderColor: [
-                'rgb(0, 82, 136)',
-                'rgb(0, 82, 136)',
-                'rgb(0, 82, 136)',
-                'rgb(0, 82, 136)',
-                'rgb(0, 82, 136)'
-            	],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    suggestedMax: 100
-                }
+window.onload = function() {
+    new Chart(document.getElementById("fuelChart"), {
+        type: 'doughnut',
+        data: {
+            labels: ["Remaining", "Used"],
+            datasets: [{
+                data: [79,21],
+                backgroundColor: ["#005288", "#DCEDF9"]
             }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            title: {
+                display: true,
+                text: 'Fuel left in percentage'
+            }
         }
-    }
-});
+    });
+
+    new Chart(document.getElementById("distanceChart"), {
+        type: 'line',
+        data: {
+            labels: [10,20,30,40,50,60,70,80,90,100],
+            datasets: [{ 
+                data: [0, 25000000, 50000000,100000000,150000000,200000000,250000000,300000000,350000000,400000000],
+                label: "Spaceship",
+                borderColor: "#005288",
+                backgroundColor: ["#DCEDF9"]
+            }]          
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            title: {
+                display: true,
+            text: 'Distance traveled over time'
+            }
+        }
+    });
+
+    new Chart(document.getElementById("supplyChart"), {
+        type: 'bar',
+   	    data: {
+            labels: ['Bread', 'Rice', 'Vegetables', 'Meat', 'Fruit'],
+            datasets: [{
+                data: [78, 76,82 ,69,75],
+                label: 'Percentage of supply left',
+                backgroundColor: [
+                    'rgb(204, 220, 231)',
+                    'rgb(204, 220, 231)',
+                    'rgb(204, 220, 231)',
+                    'rgb(204, 220, 231)',
+                    'rgb(204, 220, 231)'],
+                borderColor: [
+                    'rgb(0, 82, 136)',
+                    'rgb(0, 82, 136)',
+                    'rgb(0, 82, 136)',
+                    'rgb(0, 82, 136)',
+                    'rgb(0, 82, 136)'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        suggestedMax: 100
+                    }
+                }]
+            }
+        }
+    });
+}
